0f8d0f8ffda9a757f22e47a8ce518eac71a3f3a6
jollyjerr <jollyjerr@gmail.com>
2019-08-30 16:44:05 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(array, change) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        result.push(
+            change(array[i])
+        );
+    }
+    return result
+}
+
+function reduce(array, change, starting) {
+    let r = (!!starting) ? starting : array[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        r = change(array[i], r)
+    }
+
+    return r;
+    //yes I did copy the solution for this and I am sad about it
+}
